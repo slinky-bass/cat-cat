2951eec401b489d15e7cb08a72935eb8eee78f59
slinky-bass <danielle.lisa.eriksen@gmail.com>
2019-04-28 20:18:34 +0200

    rework main nav and list styles

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,28 +2,20 @@ import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
-// ----- INTERNAL ----- \\
-import SvgLogoIcon from '../common/SvgHomeIcon';
-import SvgHomeIcon from '../common/SvgHomeIcon';
-import SvgCatIcon from '../common/SvgHomeIcon';
-
 // ----- COMPONENTS ----- \\
 const MainNav = () => {
 
     const hashLocation = window.location.hash.substring(0, window.location.hash.indexOf("?"));
 
     return (
-        <nav className="main-nav-container">
-            <div className="logo-container">
-                <SvgLogoIcon />
-                <span></span>
-            </div>
+        <nav className="main-nav">
+            <div className="logo-container">=(^.^)=</div>
             <ul className="nav-tab-list">
-                <li className={hashLocation === "#/" ? classNames("nav-tab-item active") : classNames("nav-tab-item")}>
-                    <Link to={`/`}><SvgHomeIcon /></Link>
+                <li className={hashLocation === "/" ? classNames("nav-tab-item active") : classNames("nav-tab-item")}>
+                    <Link to={`/`}>Home</Link>
                 </li>
-                <li className={classNames("nav-tab-item", { "active": hashLocation === "#/cats" })}>
-                    <Link to={`/articles`}><SvgCatIcon /></Link>
+                <li className={classNames("nav-tab-item", { "active": hashLocation === "/articles" })}>
+                    <Link to={`/articles`}>Article</Link>
                 </li>
             </ul>
         </nav>
