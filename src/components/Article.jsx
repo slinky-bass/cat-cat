fce36c470adf0ea9cfee5dc80f53a51e3cfdceb2
slinky-bass <danielle.lisa.eriksen@gmail.com>
2019-04-29 23:36:35 +0200

    add web fonts, add article transforms

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 // ----- INTERNAL ----- \\
+import SvgCloseIcon from '../common/SvgCloseIcon';
 
 class Article extends Component {
 
@@ -16,11 +17,12 @@ class Article extends Component {
     render() {
 
         const articleContents = this.props.selectedItem !== null 
-            ? <div className="article-container">
+            ? <div className="article-container selected">
+                <div className="blackout"></div>
                 <article>
                     <header>
                         <h3>The {this.props.selectedItem.title} Cat
-                            <button type="button" className="btn-as-link" onClick={this.props.deselectListItem}>x</button>
+                            <button type="button" className="btn-as-link" onClick={this.props.deselectListItem}><SvgCloseIcon /></button>
                         </h3>        
                     </header>
                     <div className="article-contents">
@@ -32,7 +34,10 @@ class Article extends Component {
                     </div>     
                 </article>
             </div>
-            : "";
+            : <div className="article-container">
+                <div className="blackout"></div>
+                <article></article>
+            </div>;
 
         return (
             <React.Fragment>
